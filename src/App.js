7c8262d6d052af70b6c114355d36c561f17b9032
Ganesh Nr <ganesh.dev.nr@gmail.com>
2023-04-15 14:52:44 +0530

    Implemented Search Box

--- src/App.js
+++ src/App.js
@@ -1,8 +1,70 @@
+import github from "./db.js";
+import { useEffect, useState, useCallback } from "react"; // hook to perform side effects
+import query from "./Query.js";
+import RepoInfo from "./RepoInfo";
+import SearchBox from "./SearchBox";
+
+
 function App() {
+
+  let [userName, setUserName] = useState('');
+  let [repoList, setRepoList] = useState(null);
+  let [pageCount, setPageCount] = useState(10);
+  let [queryString, setQueryString] = useState("");
+  let [totalCount, setTotalCount] = useState(null);
+
+
+  const fetchData = useCallback(() => {
+
+    const queryText = JSON.stringify(query(pageCount, queryString));
+
+    fetch(github.baseURL, { 
+      method: "POST",
+      headers: github.headers,
+      body: queryText
+    })
+    .then((response => response.json()))
+    .then(data => {
+      const viewer = data.data.viewer;
+      const repos = data.data.search.nodes;
+      const total = data.data.search.repositoryCount;
+      setUserName(viewer.name);
+      setRepoList(repos);
+      setTotalCount(total);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  }, [pageCount, queryString]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill"></i> Repos</h1>
+      <p>Hey there {userName}</p>
+
+      <SearchBox
+        totalCount={totalCount}
+        pageCount={pageCount}
+        queryString={queryString}
+        onTotalChange={(total) => { setTotalCount(total)}}
+        onQueryChange={(myString) => { setQueryString(myString)}}
+        />
+      
+      { repoList && (
+        <ul className="list-group list-group-flush">
+          {
+            repoList.map((repo) => (
+              <RepoInfo key={repo.id} repo={repo} />
+            ))
+          }
+        </ul>
+      )}
     </div>
   );
 }
